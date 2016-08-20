55e2ce2e6426f19cdad556fff139e9cea85e177a
devMeex <cristhian.pinilla0@gmail.com>
2016-08-20 16:55:58 -0300

    vistas home e info

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'My New App'
+settings.subtitle = 'powered by web2py'
+settings.author = 'you'
+settings.author_email = 'you@example.com'
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '9c98725d-a089-4d6f-97fe-847d505284d0'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
